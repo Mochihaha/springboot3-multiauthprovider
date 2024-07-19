12f6456db8b61c0dd448a9c560e4b88e6a9b643b
Mochihaha <loh.ziying@gmail.com>
2024-07-19 20:52:07 +0800

    declaring authenticationProvider(s) directly in security filter chain

--- src/main/java/com/multiauthprovider/multiple_authentication_providers/SecurityConfig.java
+++ src/main/java/com/multiauthprovider/multiple_authentication_providers/SecurityConfig.java
@@ -3,10 +3,12 @@ package com.multiauthprovider.multiple_authentication_providers;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,20 +25,28 @@ import static org.springframework.security.config.Customizer.withDefaults;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http,
+                                                   PasswordEncoder passwordEncoder) throws Exception {
         http.httpBasic(withDefaults())
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(new AntPathRequestMatcher("/login")).permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .authenticationProvider(customAuthenticationProvider())
+                .authenticationProvider(inMemoryAuthenticationProvider(passwordEncoder));
 
         return http.build();
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(
-            PasswordEncoder passwordEncoder) {
-        // in-memory authentication
+    AuthenticationProvider customAuthenticationProvider() {
+        return new CustomPalindromeAuthenticationProvider();
+    }
+
+    @Bean
+    AuthenticationProvider inMemoryAuthenticationProvider(
+            PasswordEncoder passwordEncoder
+    ) {
         DaoAuthenticationProvider inMemoryAuthenticationProvider = new DaoAuthenticationProvider();
         UserDetails userDetails = User
                 .withUsername("memuser")
@@ -45,9 +55,25 @@ public class SecurityConfig {
                 .build();
         inMemoryAuthenticationProvider.setUserDetailsService(new InMemoryUserDetailsManager(userDetails));
         inMemoryAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+        return inMemoryAuthenticationProvider;
 
-        return new ProviderManager(inMemoryAuthenticationProvider, new CustomPalindromeAuthenticationProvider());
     }
+
+//    @Bean
+//    public AuthenticationManager authenticationManager(
+//            PasswordEncoder passwordEncoder) {
+//        // in-memory authentication
+//        DaoAuthenticationProvider inMemoryAuthenticationProvider = new DaoAuthenticationProvider();
+//        UserDetails userDetails = User
+//                .withUsername("memuser")
+//                .password(passwordEncoder().encode("password"))
+//                .roles("USER")
+//                .build();
+//        inMemoryAuthenticationProvider.setUserDetailsService(new InMemoryUserDetailsManager(userDetails));
+//        inMemoryAuthenticationProvider.setPasswordEncoder(passwordEncoder);
+//
+//        return new ProviderManager(inMemoryAuthenticationProvider, new CustomPalindromeAuthenticationProvider());
+//    }
 
 
     @Bean
